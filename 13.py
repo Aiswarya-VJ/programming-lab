48c9960913e7364c3c9fd1924c175c9d575f67af
Aiswarya-VJ <116331855+Aiswarya-VJ@users.noreply.github.com>
2022-11-10 22:35:27 +0530

    Delete 13.py

--- 13.py
+++
@@ -1,8 +0,0 @@
-str1=str(input("enter the word"))
-list1=list(str1)
-print(f"string is:",list1)
-print("string after exchanging the first and last characters")
-temp=list1[0]
-list1[0]=list1[-1]
-list1[-1]=temp
-print(list1)
