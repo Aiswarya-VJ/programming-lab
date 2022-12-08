58afd7242e7b6f4011d064a295fd58fc6d6c5b51
Aiswarya-VJ <116331855+Aiswarya-VJ@users.noreply.github.com>
2022-12-08 20:42:14 +0530

    Add files via upload

---
+++ 2lab1.py
@@ -0,0 +1,7 @@
+def factorial(x):
+    product=1
+    for i in range(1,x+1):
+        product=product*i
+    return(product)
+n=int(input("enter the number to find the factorial"))
+print(f"the factorial of {n} is {factorial(n)}")
